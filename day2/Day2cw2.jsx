e38b5b3f8371f3238a6cd948cdc2946cfda1ccfe
godson2005 <727723eucs201@skcet.ac.in>
2024-08-07 17:15:17 +0530

    Add files via upload

---
+++ day2/Day2cw2.jsx
@@ -0,0 +1,9 @@
+function Wel()
+{
+    return(
+        <div className="App" style={{border: '10px solid black',backgroundColor:"cyan",height:"300px",}}>
+            <h1 style={{color:"green"}}>Let we see the output of JAVASCRIPT</h1>
+            <p style={{color:"darkblue"}}>lorem epsum dolit van der dussen</p>
+        </div>
+    )
+}export default Wel;
